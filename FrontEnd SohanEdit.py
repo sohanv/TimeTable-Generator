a6d3075b76713aab6bacad111fe8a2623bdef9e4
AdhvaithRajesh <adhvaithrajesh23@gmail.com>
2022-01-18 11:11:06 +0530

    instructions added
    I've added the instructions button and the file, not opening on my system

--- FrontEnd SohanEdit.py
+++ FrontEnd SohanEdit.py
@@ -1,17 +1,18 @@
-from ctypes import alignment
+#from ctypes import alignment
 from tkinter import *
-from turtle import right #importing tkinter
+#from turtle import right #importing tkinter
 from PIL import ImageTk, Image
-from numpy import left_shift
+#from numpy import left_shift
 from timetable import timetable_main as tt
 from subjects_file import enter, export
 from download import download_button
+import os
 
 #root window
 root = Tk()
 root.title('Timetable Generator')
 root.configure(bg = 'black')
-root.iconbitmap("ad_cooldude.ico")
+#root.iconbitmap("ad_cooldude.ico")
 #root.attributes('-fullscreen', True)
 
 #functions
@@ -19,7 +20,7 @@ def display_timetable(): #function to open a pop-up window
     pop_up = Toplevel()
     pop_up.title('Timetable Generator')
     pop_up.configure(bg = 'black')
-    pop_up.grid
+    pop_up.grid()
 
     days = int(daysEntry.get())     # sohan added
     #periods = int(periodsEntry.get())
@@ -35,19 +36,29 @@ def display_timetable(): #function to open a pop-up window
     image_label = Label(pop_up, image = timetable_img)
     image_label.grid(row = 0, column = 0)
 
+def display_instructions():
+    pop_up2 = Toplevel()
+    pop_up2.title('Timetable Generator Instructions')
+    pop_up2.configure(bg = 'black')
+    pop_up2.grid()
+    f = open('Instructions.txt', 'r')
+    text_label = Label(pop_up2, text = f.read())
+    
 #Labels and all that, 'na mean?
-daysLabel = Label(root, text = 'Enter number of days')
+daysLabel = Label(root, text = 'Enter number of days', fg = 'white', bg = 'black')
 
 #periodsLabel = Label(root, text = 'Enter number of periods per day')
 
-subjectsLabel = Label(root, text = 'Enter the subjects seperated by a comma') # sohan added
+subjectsLabel = Label(root, text = 'Enter the subjects seperated by a comma', fg = 'white', bg = 'black') # sohan added
 
 #Buttons
 generate = Button(root, text = 'Generate Timetable', bg = 'green', fg = 'white', command = display_timetable)
 
 download = Button(root, text = 'Download TimeTable to Desktop', bg = 'green', fg = 'white', command = download_button)
 
-quit = Button(root, text = 'QUIT', bg = 'red', fg = 'white', command = root.destroy )
+quit = Button(root, text = '                       QUIT                       ', bg = 'red', fg = 'white', command = root.destroy)
+
+instructions = Button(root, text = '       Instructions       ', bg = 'green', fg = 'white', command = display_instructions)
 
 #taking input 
 daysEntry = Entry(root, text = 'Enter number of days', width = 12, fg = 'black', bg = 'grey', bd = 5)
@@ -69,10 +80,12 @@ subjectsLabel.grid(row = 1, column = 0)
 
 subjectsEntry.grid(row = 1, column = 1, padx = 35, pady = 10, columnspan = 3)
 
+instructions.grid(row = 3, column = 0, padx = 20, pady = 20, columnspan = 1)
+
 generate.grid(row = 2, column = 0, padx = 20, pady = 10, columnspan = 1)
 
 download.grid(row = 2, column = 1, padx = 20, pady = 20, columnspan = 1)
 
-quit.grid(row = 3, column = 1, padx = 20, pady = 20, columnspan = 2)
+quit.grid(row = 3, column = 1, padx = 20, pady = 20, columnspan = 1)
 
 root.mainloop()
